7eacc95fa953a21ee4321b9220dfda090f0a13f6
Steven Schulte <spschul@umich.edu>
2019-11-20 15:02:42 -0500

    mapping is working??

--- src/slam/mapping.cpp
+++ src/slam/mapping.cpp
@@ -48,15 +48,26 @@ void Mapping::updateMap(const lidar_t& scan, const pose_xyt_t& pose, OccupancyGr
         last_pose = pose;
         return;
     }
+    int l0 = 0;
     MovingLaserScan lidar_rays = MovingLaserScan(scan, last_pose, pose);
     for (const adjusted_ray_t& ray : lidar_rays) {
         Point<float> vec { ray.range * std::cos(ray.theta), ray.range * std::sin(ray.theta) };
         Point<float> endpoint = ray.origin + vec;
         Point<int> ray_start_grid = global_position_to_grid_cell(ray.origin, map);
         Point<int> ray_end_grid = global_position_to_grid_cell(endpoint, map);
-        for (const Point<int>& grid_cell : bresenham(ray_start_grid, ray_end_grid, map)) {
-            map.setLogOdds(grid_cell.x, grid_cell.y, 127); // TODO not correct
+        std::vector<Point<int>> bresenham_path = bresenham(ray_start_grid, ray_end_grid, map);
+        for (auto it = bresenham_path.cbegin(); it < bresenham_path.cend() - 1; ++it) {
+            const Point<int>& grid_cell = *it;
+            int current_log_odds = map.logOdds(grid_cell.x, grid_cell.y);
+            current_log_odds += -kMissOdds_ - l0;
+            current_log_odds = std::min(std::max(current_log_odds, -128), 127);
+            map.setLogOdds(grid_cell.x, grid_cell.y, current_log_odds);
         }
+        const Point<int>& grid_cell = *bresenham_path.crbegin();
+        int terminal_log_odds = map.logOdds(grid_cell.x, grid_cell.y);
+        terminal_log_odds += kHitOdds_ - l0;
+        terminal_log_odds = std::min(std::max(terminal_log_odds, -128), 127);
+        map.setLogOdds(grid_cell.x, grid_cell.y, terminal_log_odds);
     }
     last_pose = pose;
 }
