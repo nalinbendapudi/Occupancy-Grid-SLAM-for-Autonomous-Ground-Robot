ff3b8f3271e7497256fa4d211221d1f49eb555a2
Tor <tors@umich.edu>
2019-12-04 00:55:11 -0500

    finish writing A*, need to debug

--- src/planning/astar.cpp
+++ src/planning/astar.cpp
@@ -1,5 +1,6 @@
 #include <planning/astar.hpp>
 #include <planning/obstacle_distance_grid.hpp>
+#include <queue>
 
 
 robot_path_t search_for_path(pose_xyt_t start, 
@@ -9,9 +10,113 @@ robot_path_t search_for_path(pose_xyt_t start,
 {
     ////////////////// TODO: Implement your A* search here //////////////////////////
     
+    // 3D configuration map for closed list
+    pose_xyt_t initial;
+    initial.utime = -1;
+    std::vector<std::vector<pose_xyt_t>> closed_list(distances.heightInCells(),std::vector<pose_xyt_t>(distances.widthInCells(),initial));
+
+    // open list
+    std::priority_queue<Node> open_list;
+
+    // add start node to openlist
+    Node start_node;
+    start_node.self = start;
+    start_node.parent = start;
+    start_node.g = 0;
+    open_list.push(start_node);
+
+    // create current node
+    Node current_node;
+
+    // create path
     robot_path_t path;
-    path.utime = start.utime;
-    path.path.push_back(start);    
+
+    // check if open list is empty
+    while(!open_list.empty()){
+        
+        std::cout << "in loop" << std::endl;
+
+        // get current node in open list
+        current_node = open_list.top();
+        open_list.pop();
+
+        // insert current node to closed list
+        closed_list[int(current_node.self.x/distances.metersPerCell())][int( \
+                    current_node.self.y/distances.metersPerCell())] = current_node.parent;
+
+        // Goal check
+        if (sqrt((current_node.self.x - goal.x)*(current_node.self.x - goal.x)+ \
+            (current_node.self.y - goal.y)*(current_node.self.y - goal.y)) < 0.01)
+        {         
+            path.path.push_back(goal);
+
+            // parent node
+            pose_xyt_t parent;
+            pose_xyt_t current = current_node.self;
+
+            // back trace to start
+            while (sqrt((current.x - start.x)*(current.x - start.x)+ \
+                    (current.y - start.y)*(current.y - start.y)) > 0.01)
+            {
+                parent = closed_list[int(current.x/distances.metersPerCell())][int(current.y/distances.metersPerCell())];
+                parent.theta = atan2(current.y - parent.y, current.x - parent.x);
+                path.path.push_back(parent);
+                current = parent;
+            }
+
+            std::reverse(path.path.begin(),path.path.end());
+        }
+
+        // expand current node
+        for(float x = current_node.self.x - distances.metersPerCell();x <= current_node.self.x + distances.metersPerCell(); x=x+distances.metersPerCell())
+        {
+            for(float y = current_node.self.y - distances.metersPerCell();y <= current_node.self.y + distances.metersPerCell(); y=y+distances.metersPerCell())
+            {
+                // check if the child is current node
+                if (sqrt((current_node.self.x - x)*(current_node.self.x - x)+ \
+                         (current_node.self.y - y)*(current_node.self.y - y) < 0.01)) continue;
+
+                // check if the child is in grid
+                if (!distances.isCellInGrid(int(x/distances.metersPerCell()), \
+                    int(y/distances.metersPerCell()))) continue;
+
+                // check if the child is an obstacle
+                float obsDistance = distances(int(x/distances.metersPerCell()),int(y/distances.metersPerCell()));
+                if (obsDistance <= params.minDistanceToObstacle) continue;
+
+                // check if the child has been explored
+                if (closed_list[int(x/distances.metersPerCell())][int(y/distances.metersPerCell())].utime != -1) continue;
+
+                // add child to open list
+                Node child;
+                pose_xyt_t child_pose;
+                child_pose.x = x;
+                child_pose.y = y;
+
+                child.self = child_pose;
+                child.parent = current_node.self;
+                child.g = current_node.g + sqrt((x-current_node.self.x)*(x-current_node.self.x)+(y-current_node.self.y)*(y-current_node.self.y));
+                child.f = child.g + sqrt((x-goal.x)*(x-goal.x)+(y-goal.y)*(y-goal.y)) + \
+                            (obsDistance < params.maxDistanceWithCost) ? \
+                            obsDistance + std::pow(params.maxDistanceWithCost - obsDistance, params.distanceCostExponent): obsDistance;
+
+                open_list.push(child);
+            }
+        } 
+    }
+    
+    
+    path.utime = start.utime;    
     path.path_length = path.path.size();
     return path;
+}
+
+void round_theta(float& theta)
+{
+    if(theta < 0){
+        theta += 2 * M_PI;
+    }
+    if(theta > 2 * M_PI){
+        theta -= 2 * M_PI;
+    }
 }
